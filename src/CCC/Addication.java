456e3d2e1b685b6179cb85ab661efe3f92bf6bad
DELL <DELL@DESKTOP-997ERFS>
2022-03-03 23:26:20 +0530

    Even NUmber

---
+++ src/CCC/Addication.java
@@ -0,0 +1,18 @@
+package CCC;
+
+public class Addication {
+
+	public static void main(String[] args) {
+		int a=10;
+		if(a%2==0)
+		{
+			System.out.println("Number is Even--->"+a);
+		}
+		else
+		{
+			System.out.println("Number is ODd--->"+a);
+		}
+
+	}
+
+}
